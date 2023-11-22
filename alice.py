de956959fc547a5fda5a5e6e2fe1c1c5bc295300
thinkty <the.thinkty@gmail.com>
2023-11-22 13:54:29 -0500

    Added code for alice and bob

--- alice.py
+++ alice.py
@@ -1,19 +1,32 @@
 
-from cryptography.hazmat.primitives.asymmetric import rsa
+import argparse
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+import hashlib
 import socketserver
 import sys
+
+responses = []
+public_key_pem = None
 
 class SignatureRequestHandler(socketserver.BaseRequestHandler):
     def handle(self) -> None:
         data = self.request.recv(1024)
-        text = data.decode('utf-8')
+        segnum = int(data.decode('utf-8'))
 
-        # TODO: request should be a number indicating the segment number (make sure the segment number is in bound)
+        # Get segment number from the request (between 0 and 4)
+        if segnum < 0 or segnum > 4 or public_key_pem is None or len(responses) != 5:
+            self.request.send('ERR'.encode('utf-8'))
+            self.request.close()
+            return
 
-        # TODO: sign the requested segment's digest and send public keys with it
-
-        print("alice < {}".format(text))
-        self.request.send('OK'.encode('utf-8'))
+        # Send the public key, message, and signature
+        print("Sending segment {}".format(segnum))
+        # print(len(public_key_pem))
+        # print(len(responses[segnum][0]))
+        # print(len(responses[segnum][1]))
+        self.request.sendall(public_key_pem + responses[segnum][0] + responses[segnum][1])
         self.request.close()
 
 class SignatureServer(socketserver.TCPServer):
@@ -24,10 +37,63 @@ class SignatureServer(socketserver.TCPServer):
         super().__init__(server_address, RequestHandlerClass, bind_and_activate)
 
 if __name__ == "__main__":
-    
-    # TODO: generate RSA key pair
 
-    # TODO: compute digest of each segment 
+    # Parse the segment names
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', metavar='segments', type=str, nargs='+', help='File name of 5 segments')
+    args = parser.parse_args()
+    segments = args.files
+    if len(segments) != 5:
+        print('error: must specify 5 segment files...')
+        exit(1)
+
+    # Compute digest of each segment
+    digests = []
+    for name in segments:
+        try:
+            sha256 = hashlib.sha256()
+            with open(name, 'rb') as segment:
+                for chunk in iter(lambda: segment.read(4096), b''):
+                    sha256.update(chunk)
+            digests.append(sha256.hexdigest())
+        except FileNotFoundError:
+            print('error: segment {} not found...'.format(name))
+            exit(1)
+        except Exception as e:
+            print('error: {}'.format(e))
+            exit(1)
+    print('Digest of segments:')
+    for i, digest in enumerate(digests):
+        print('{}: {}'.format(i, digest))
+
+    # Generate RSA key pair
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+    public_key = private_key.public_key()
+    
+    # Serialize the public key into PEM format to be sent to any requester
+    public_key_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+
+    # Sign each segment and create responses in advance
+    for i, segment_digest in enumerate(digests):
+        # Prepend the segment index so that if the adversary changes Bob's request, Bob will know
+        message = (str(i) + segment_digest).encode('utf-8')
+        # Signing will pad the message if necessary and hash the padded message again before signing
+        signature = private_key.sign(
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        responses.append((message, signature))
 
     server = SignatureServer(('localhost', 55555), SignatureRequestHandler)
     try:
--- bob.py
+++ bob.py
@@ -1,17 +1,91 @@
 
+import argparse
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
 import socket
+import hashlib
+
+server_addr = 'localhost'
+server_port = 55555
+
+def connectAndRequest(segnum: int) -> str:
+    # Connect to alice
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientsocket.connect((server_addr, server_port))
+
+    # Send the request
+    clientsocket.send(str(segnum).encode('utf-8'))
+
+    # Receive public key, message, and signature
+    public_key_pem = clientsocket.recv(451)
+    message = clientsocket.recv(65) # 1 byte of segnum, 32 bytes (256 bits) of digest (but x2 since hex)
+    signature = clientsocket.recv(256) # signature of 2048 bit key size has 256 bytes
+    clientsocket.close()
+
+    # Verify assuming that the adversary cannot modify public key
+    public_key = serialization.load_pem_public_key(public_key_pem)
+    try:
+        public_key.verify(
+            signature,
+            message,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+        return message.decode('utf-8')
+    except Exception as e:
+        print('error: {}'.format(e))
+        return ''
+
 
 if __name__ == "__main__":
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect(('localhost', 55555))
 
-    # TODO: compute digest of the segments
+    # Parse the segment names
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', metavar='segments', type=str, nargs='+', help='File name of 5 segments')
+    args = parser.parse_args()
+    segments = args.files
+    if len(segments) != 5:
+        print('error: must specify 5 segment files...')
+        exit(1)
 
-    # TODO: request alice with a specific segment number
+    # Compute digest of the segments
+    digests = []
+    for name in segments:
+        try:
+            sha256 = hashlib.sha256()
+            with open(name, 'rb') as segment:
+                for chunk in iter(lambda: segment.read(4096), b''):
+                    sha256.update(chunk)
+            digests.append(sha256.hexdigest())
+        except FileNotFoundError:
+            print('error: segment {} not found...'.format(name))
+            exit(1)
+        except Exception as e:
+            print('error: {}'.format(e))
+            exit(1)
 
-    message = 'hello'
-    print("bob > {}".format(message))
-    clientsocket.send(message.encode('utf-8'))
-    data = clientsocket.recv(1024)
-    text = data.decode('utf-8')
-    print("bob < {}".format(text))
+    # Make a request for each segment and compare
+    matches = 0
+    for segnum in range(5):
+        print('Requesting segment {}'.format(segnum))
+        message = connectAndRequest(segnum)
+        if message == '':
+            print('error: failed to verify received message...')
+            exit(1)
+
+        # Check that the requested segnum is the same as received
+        if segnum != int(message[:1]):
+            print('error: different segnum received...')
+            exit(1)
+
+        # Check if there is a match with the received digest
+        digest = message[1:]
+        for i in range(5):
+            if digests[i] == digest:
+                matches += 1
+                break
+    
+    print('Matches found: {}'.format(matches))
